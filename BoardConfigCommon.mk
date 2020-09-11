c19cb9f2903d29901950490c163e545d9ea8726e
allerttt <thesupernikkce@yandex.ru>
2020-09-11 21:01:56 +0300

    BoardConfig: Enable BUILD_BROKEN_DUP_RULES

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,6 +20,8 @@ PLATFORM_PATH := device/xiaomi/msm8998-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
 
+BUILD_BROKEN_DUP_RULES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
